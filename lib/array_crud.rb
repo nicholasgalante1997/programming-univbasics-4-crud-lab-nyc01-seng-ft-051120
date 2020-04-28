738c9d183e4ae937551c5bc4b3bb53c673256d1e
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-28 11:26:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,8 +26,9 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["v", "r" "am", ]
-  return array[2]
+  array = ["and", "the", "am", "it"]
+  index_number = array[2]
+  p index_number
 end
 
 def retrieve_first_element_from_array(array)
